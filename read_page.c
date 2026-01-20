aa740cab002569c5702e8f6604fc05230f2f9b01
Fredrik Wilke <fwilke@bu.edu>
2026-01-20 23:48:21 +0100

    [mod:] read_page improved output format

--- read_page.c
+++ read_page.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<time.h>
 #include<unistd.h>
+#include<string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -61,19 +62,29 @@ int main(int argc, char *argv[]) {
     struct stat f_map_stat;
     size_t pg_size = sysconf(_SC_PAGESIZE);
     char buff[pg_size];
+    bool verbose = false;
+    int arg_idx = 1;
+    
+    // Declare timing variables at function scope
+    clock_t seek_begin = 0, seek_end = 0;
+    uint64_t seek_begin_ns = 0, seek_end_ns = 0;
+    clock_t map_end = 0;
+    uint64_t map_end_ns = 0;
 
-    printf("page size = %zu\n", pg_size);
+    // Check for -v flag
+    if (argc >= 2 && strcmp(argv[1], "-v") == 0) {
+        verbose = true;
+        arg_idx = 2;
+    }
 
-    if (argc < 2) {
-        printf("Needs 1 arg.\n");
+    if (argc < arg_idx + 1) {
+        fprintf(stderr, "Usage: %s [-v] <file> [page_number]\n", argv[0]);
         exit(EBADF);
     }
     
-    if (argc == 3) {
-	page_to_read = atoi(argv[2]);
-    } 
-
-    printf("argv[1] = %s\n", argv[1]);
+    if (argc == arg_idx + 2) {
+	page_to_read = atoi(argv[arg_idx + 1]);
+    }
 
     //warm up timers
     CLOCK_FUNC();
@@ -84,17 +95,13 @@ int main(int argc, char *argv[]) {
     //time the open
     clock_t open_begin = COUNTER_FUNC();
     uint64_t open_begin_ns = CLOCK_FUNC();
-    f_map = open(argv[1], O_RDONLY );//| O_DIRECT | O_SYNC);
+    f_map = open(argv[arg_idx], O_RDONLY );//| O_DIRECT | O_SYNC);
     clock_t open_end = COUNTER_FUNC();
     uint64_t open_end_ns = CLOCK_FUNC();
-    printf("Open time: %lu, %lu, ratio=%f\n",
-           (open_end - open_begin),
-           (open_end_ns - open_begin_ns),
-           (double)(open_end - open_begin) / (double)(open_end_ns - open_begin_ns));
 
 
     if (f_map == -1) {
-        printf("Failed to open file %s\n", argv[1]);
+        fprintf(stderr, "Failed to open file %s\n", argv[arg_idx]);
         exit(errno);
     }
 
@@ -103,13 +110,8 @@ int main(int argc, char *argv[]) {
     fstat(f_map, &f_map_stat);
     clock_t stat_end = COUNTER_FUNC();
     uint64_t stat_end_ns = CLOCK_FUNC();
-    printf("Stat time: %lu, %lu, ratio=%f\n",
-           (stat_end - stat_begin),
-           (stat_end_ns - stat_begin_ns),
-           (double)(stat_end - stat_begin) / (double)(stat_end_ns - stat_begin_ns));
 
     file_pgs = (f_map_stat.st_size + (pg_size - 1)) / pg_size;
-    printf("file includes %lu pages\n", file_pgs);
 
     // The arguments to mmap are not of a great importance, we will
     // use these pages just to spy on them. What we try to do here
@@ -124,26 +126,17 @@ int main(int argc, char *argv[]) {
     void *mapped_to = mmap(NULL, f_map_stat.st_size,
                            PROT_EXEC, MAP_SHARED, f_map, 0);
 
-    clock_t map_end = COUNTER_FUNC();
-    uint64_t map_end_ns = CLOCK_FUNC();
-        printf("Mapping time: %lu, %lu, ratio=%f\n",
-            (map_end - map_begin),
-            (map_end_ns - map_begin_ns),
-            (double)(map_end - map_begin) / (double)(map_end_ns - map_begin_ns));
+    map_end = COUNTER_FUNC();
+    map_end_ns = CLOCK_FUNC();
 #endif
-    if (argc == 3)
+    if (argc == arg_idx + 2)
     {
-        clock_t seek_begin = COUNTER_FUNC();
-        uint64_t seek_begin_ns = CLOCK_FUNC();
+        seek_begin = COUNTER_FUNC();
+        seek_begin_ns = CLOCK_FUNC();
         file_pgs = 1;
 	    lseek(f_map, pg_size * page_to_read, SEEK_SET);
-        clock_t seek_end = COUNTER_FUNC();
-        uint64_t seek_end_ns = CLOCK_FUNC();
-        printf("Seeked to pos 0x%llx -> page %d\n", (unsigned long long)(pg_size * page_to_read), page_to_read);
-        printf("Seek time: %lu, %lu, ratio=%f\n",
-               (seek_end - seek_begin),
-               (seek_end_ns - seek_begin_ns),
-               (double)(seek_end - seek_begin) / (double)(seek_end_ns - seek_begin_ns));
+        seek_end = COUNTER_FUNC();
+        seek_end_ns = CLOCK_FUNC();
     }
     
     for (size_t i = 0; i < file_pgs; i++)
@@ -158,26 +151,53 @@ int main(int argc, char *argv[]) {
         clock_t end = COUNTER_FUNC();
         uint64_t end_ns = CLOCK_FUNC();
         
-        // calculate elapsed time by finding difference (end - begin) and
-        // dividing the difference by CLOCKS_PER_SEC to convert to seconds
-        //time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-        //printf("The elapsed time is %f seconds", time_spent);
-         printf("read time: %lu, %lu, ratio=%f\n",
-             (end - begin),
-             (end_ns - begin_ns),
-             (double)(end - begin) / (double)(end_ns - begin_ns));
-
         usleep(3000);
 
     }
-    clock_t end = COUNTER_FUNC();
-    uint64_t end_ns = CLOCK_FUNC();
-        printf("Total time: %lu, %lu, ratio=%f\n",
-            (end - map_begin),
-            (end_ns - map_begin_ns),
-            (double)(end - map_begin) / (double)(end_ns - map_begin_ns));
+    clock_t total_end = COUNTER_FUNC();
+    uint64_t total_end_ns = CLOCK_FUNC();
 
-    printf("\n");
+    // Print CSV header if verbose
+    if (verbose) {
+        printf("page_size,filename,open_cycles,open_ns,open_ratio,stat_cycles,stat_ns,stat_ratio,file_pages");
+#if MMAP_FILE
+        printf(",map_cycles,map_ns,map_ratio");
+#endif
+        if (argc == arg_idx + 2) {
+            printf(",seek_pos,page_number,seek_cycles,seek_ns,seek_ratio");
+        }
+        printf(",total_cycles,total_ns,total_ratio\n");
+    }
+
+    // Print CSV data row
+    printf("%zu,%s,%lu,%lu,%f,%lu,%lu,%f,%lu",
+           pg_size,
+           argv[arg_idx],
+           (open_end - open_begin),
+           (open_end_ns - open_begin_ns),
+           (double)(open_end - open_begin) / (double)(open_end_ns - open_begin_ns),
+           (stat_end - stat_begin),
+           (stat_end_ns - stat_begin_ns),
+           (double)(stat_end - stat_begin) / (double)(stat_end_ns - stat_begin_ns),
+           file_pgs);
+#if MMAP_FILE
+    printf(",%lu,%lu,%f",
+           (map_end - map_begin),
+           (map_end_ns - map_begin_ns),
+           (double)(map_end - map_begin) / (double)(map_end_ns - map_begin_ns));
+#endif
+    if (argc == arg_idx + 2) {
+        printf(",0x%llx,%d,%lu,%lu,%f",
+               (unsigned long long)(pg_size * page_to_read),
+               page_to_read,
+               (seek_end - seek_begin),
+               (seek_end_ns - seek_begin_ns),
+               (double)(seek_end - seek_begin) / (double)(seek_end_ns - seek_begin_ns));
+    }
+    printf(",%lu,%lu,%f\n",
+           (total_end - map_begin),
+           (total_end_ns - map_begin_ns),
+           (double)(total_end - map_begin) / (double)(total_end_ns - map_begin_ns));
 
     fflush(stdout);
 
